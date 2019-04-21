b00b0e245f291cc09b3503450bd2c705916051ca
benBen <benmbenjamin@gmail.com>
2019-04-21 19:07:37 -0400

    Updates

---
+++ precache-manifest.575a0d1d1ecf69acaf51b5affc5894a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57b4327bd56f7acfdaf1",
+    "url": "/h-challenge-react/static/js/runtime~main.d1db632a.js"
+  },
+  {
+    "revision": "672c580645505d84169f",
+    "url": "/h-challenge-react/static/js/main.dcf422d5.chunk.js"
+  },
+  {
+    "revision": "5a89fe69e048f022fdc2",
+    "url": "/h-challenge-react/static/js/2.3beedbfc.chunk.js"
+  },
+  {
+    "revision": "672c580645505d84169f",
+    "url": "/h-challenge-react/static/css/main.7689cd9f.chunk.css"
+  },
+  {
+    "revision": "7353a37035f44bab54f0e4c0d83487a9",
+    "url": "/h-challenge-react/index.html"
+  }
+];
